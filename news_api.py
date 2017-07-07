90f8b826b70dfc09c38aac13d0b5824a078001ec
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-07-06 17:44:46 -0700

    news_api: api call & count word occurrence
    - call to newsAPI to get cnn data -> top news articles
    - function to count how often each word occurs

---
+++ news_api.py
@@ -0,0 +1,36 @@
+"""api calls to news api for cnn, breitbart, new york times"""
+
+
+import requests
+
+
+def cnn(api_key):
+    """make api call for cnn, return body of text with top news"""
+
+    cnn = requests.get("https://newsapi.org/v1/articles?source=cnn&"+
+                       "sortBy=top&apiKey="+api_key)
+
+    cnn_articles = cnn.json()["articles"]
+
+    cnn_text = ""
+
+    for article in cnn_articles:
+
+        if article["title"]:
+            cnn_text += article["title"] + " "
+        if article["description"]:
+            cnn_text += article["description"] + " "
+
+    return cnn_text
+
+
+def count_words(str):
+    """create dictionary with word and occurrence"""
+
+    word_count = {}
+
+    for word in str.split():
+        word_count[word] = word_count.get(word, 0)
+        word_count[word] += 1
+
+    return word_count
